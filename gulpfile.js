b5beb1a85e833894a0bce617960b788db62e9c81
Michael Zhang <me@michaelz.xyz>
2016-08-06 22:15:41 -0500

    Minified css resources too

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp");
-var concat = require("gulp-concat");
+var cssmin = require("gulp-cssmin");
+var concatcss = require("gulp-concat-css");
+var concatjs = require("gulp-concat");
+var rename = require("gulp-rename");
 var uglify = require("gulp-uglifyjs");
 
-gulp.task("concat", function() {
+gulp.task("concat-js", function() {
 	gulp.src([
 		"js/src/jquery.js",
 		"js/src/moment.js",
@@ -13,16 +16,29 @@ gulp.task("concat", function() {
 		"js/src/jquery.cookie.js",
 		"js/src/main.js",
 		])
-		.pipe(concat("trucksu.js"))
+		.pipe(concatjs("trucksu.js"))
 		.pipe(gulp.dest("js"));
 });
 
-gulp.task("uglify", function() {
+gulp.task("uglify-js", function() {
 	gulp.src("js/trucksu.js")
 		.pipe(uglify("trucksu.min.js", { "mangle": false }))
 		.pipe(gulp.dest("js"));
 });
 
+gulp.task("concat-css", function() {
+	gulp.src("css/src/*.css")
+		.pipe(concatcss("trucksu.css"))
+		.pipe(gulp.dest("css"));
+});
+
+gulp.task("minify-css", function() {
+	gulp.src("css/trucksu.css")
+		.pipe(cssmin())
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(gulp.dest("css"));
+});
+
 gulp.task("default", function() {
-	gulp.start("concat", "uglify");
+	gulp.start("concat-js", "uglify-js", "concat-css", "minify-css");
 });
